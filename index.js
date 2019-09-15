d31b85124d309f72df17f796419ff224b052058c
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-09-15 13:56:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code here!
+
+// 1) index.html
+//      after index.js is processed
+//      no longer has DOM node 'main#main':
+let main = document.querySelector('main');
+main.remove()
+
+// 2) index.html
+//      after index.js is processed
+//      has a 'newHeader' variable that points to node 'h1#victory':
+// 3) index.html
+//      after index.js is processed
+//      has a 'newHeader' variable that points to node 'h1#victory':
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory')
+
+
+// 4) index.html
+//      after index.js is processed
+//      has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside:
+document.querySelector('body').appendChild(newHeader)
+newHeader.innerHTML = 'Hemesh is the champion'
