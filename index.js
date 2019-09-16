61d6d457ded4a4aa7e1006de9d54f5996313c605
Cody <lawdermiltcody@gmail.com>
2019-09-16 13:45:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// Write your code here!let ul = document.createElement('ul')
+let main = document.querySelector('main');
+main.remove()
+
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory')
+
+
+document.querySelector('body').appendChild(newHeader)
+newHeader.innerHTML = "Cody is the champion"
