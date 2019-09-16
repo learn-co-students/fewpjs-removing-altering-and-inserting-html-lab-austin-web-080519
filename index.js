71f35d1aa03353c596d6a3618eb14683ad4618b0
Jason <jas0nle@yahoo.com>
2019-09-15 21:47:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+
 // Write your code here!
+
+let main = document.querySelector("main")
+main.remove();
+
+let element = document.createElement('div')
+document.body.appendChild(element);
+
+let newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory");
+
+
+newHeader.innerHTML = "is the champion"
+
+
