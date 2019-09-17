556c46021259d118fea81cd08685df936865ff7e
Syd <green@Syds-MacBook-Air-2.local>
2019-09-17 12:28:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let mainId = document.querySelector('main')
+mainId.remove()
+
+let newHeader = document.createElement ('h1')
+    newHeader.setAttribute("id", "victory")
+let sweetMessage = document.createTextNode ('is the champion')
+
+    document.body.appendChild(newHeader)
+    newHeader.appendChild(sweetMessage)
